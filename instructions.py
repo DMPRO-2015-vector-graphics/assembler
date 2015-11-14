ec5cae70042306c5975eaa7bbf63d9895e4d1bd4
Jonatan Lund <jonatan.lund.92@gmail.com>
2015-11-14 17:38:37 +0100

    Add more instructions, and metadata

--- instructions.py
+++ instructions.py
@@ -1,40 +1,56 @@
 class Instructions(object):
     instructions = {
         'nop': {
-            'opcode': '000000'
+            'opcode': '000000',
+            'word_partition': [(0, 32)]
         },
         'jmp': {
-            'opcode': '000001'
+            'opcode': '000001',
+            'word_partition': [(0, 6), (1, 26)]
         },
         'mov': {
-            'opcode': '000010'
+            'opcode': '000010',
+            'word_partition': [(0, 6), (1, 5), (2, 21)]
         },
         'add': {
-            'opcode': '000011'
+            'opcode': '000011',
+            'word_partition': [(0, 6), (1, 5), (2, 5), (3, 5)]
         },
         'lsl': {
-            'opcode': '000100'
+            'opcode': '000100',
+            'word_partition': [(0, 6), (1, 5), (2, 5), (3, 16)]
         },
         'line': {
-            'opcode': '000101'
+            'opcode': '000101',
+            'word_partition': [(0, 6), (1, 10), (2, 5), (3, 11)]
         },
         'bezquad': {
-            'opcode': '000110'
+            'opcode': '000110',
+            'word_partition': [(0, 6), (1, 10), (2, 5), (3, 5), (4, 6)]
         },
         'bezqube': {
-            'opcode': '000111'
+            'opcode': '000111',
+            'word_partition': [(0, 6), (1, 10), (2, 5), (3, 5), (4, 1)]
         },
         'ldr': {
-            'opcode': '001000'
+            'opcode': '001000',
+            'word_partition': [(0, 6), (1, 5), (2, 21)]
         },
         'str': {
-            'opcode': '001001'
+            'opcode': '001001',
+            'word_partition': [(0, 6), (1, 5), (2, 21)]
         },
         'ldrp': {
-            'opcode': '001010'
+            'opcode': '001010',
+            'word_partition': [(0, 6), (1, 26)]
         },
         'strp': {
-            'opcode': '001011'
+            'opcode': '001011',
+            'word_partition': [(0, 6), (1, 26)]
+        },
+        'beq': {
+            'opcode': '001100',
+            'word_partition': [(0, 6), (1, 5), (2, 5), (3, 16)]
         }
     }
 
@@ -42,3 +58,8 @@ class Instructions(object):
     def get_opcode(instruction):
         value = Instructions.instructions[instruction]
         return value['opcode']
+
+    @staticmethod
+    def get_word_partitioning(instruction):
+        value = Instructions.instructions[instruction]
+        return value['word_partition']
